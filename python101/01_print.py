76b5dade196e8d10be40b06717fdd37e357b9b77
Yeimi-A-Ruiz <yarp23@gmail.com>
2023-06-01 22:01:47 -0500

    New files

---
+++ python101/01_print.py
@@ -0,0 +1,19 @@
+print("Hello world, this is python")
+print("Let me introduce my selft, My name is Yeimi Andres")
+
+#This is a comment and python does not use it to run the program
+#Python allows you to do mathematical operations
+
+print(12 + 5)
+print(10 + 5)
+print(2 * 3)
+print(8 / 2)
+
+"""
+This is another way to put comments in the program.
+In this case, it is possible to write as many lines as you need to write
+perhaps for more specific instructions. 
+"""
+'''
+This is another way to 
+'''
---
+++ python101/02_vars.py
@@ -0,0 +1,13 @@
+#variables
+
+#This is a STR variable
+
+my_name = "Yeimi"
+print(my_name)
+
+#This is a number variable
+
+my_age = 12
+print(my_age)
+
+my_name = "Andres"
